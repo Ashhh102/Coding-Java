8b07eb45f72ea6b9b73eb3beb88cc2caf103789c
Ashwin Karthikeyan <ashwin.karthikeyan2005@gmail.com>
2026-08-25 12:06:56 +0530

    Force committed solution

---
+++ 703836-max-sum-subarray-of-size-k-GFG/703836-max-sum-subarray-of-size-k.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxSubarraySum(int[] arr, int k) {
+        // Code here
+        int sum = 0;
+        
+        for(int i = 0; i< k; i++){
+            sum += arr[i] ;
+        }
+        int max=sum;
+        for(int i = k; i<arr.length; i++){
+            sum +=arr[i];
+            sum -= arr[i-k];
+            max=Math.max(max,sum);
+            
+        }
+        
+        return max;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
